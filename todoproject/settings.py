f78003d3cd7e7f8779bdb0f63d0fc2fda29d6334
mawisirebenson4-jpg <mawisirebenson4@gmail.com>
2026-04-30 11:05:22 +0100

    "Final"

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^$19gx#!-mq42=lvhm-v2+))fv1v3squp3i*h5lr#v4c-(mu9$'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-^$19gx#!-mq42=lvhm-v2+))fv1v3squp3i*h5lr#v4c-(mu9$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 
 # Application definition
@@ -116,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
